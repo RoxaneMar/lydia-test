c0ffb63bcc882d7b3f06fa72588a950101b44111
Roxane Marcilhacy <roxane.marcilhacy@gmail.com>
2017-07-04 10:57:19 +0200

    useless comments and bad indentation in Receiver.php

--- app/Receiver.php
+++ app/Receiver.php
@@ -1,33 +1,20 @@
 <?php
 
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
   class Receiver extends Model {
-
-
     protected $fillable = array('firstname', 'lastname', 'email', 'amount', 'error', 'request_id', 'message');
 
-      public $firstname;
-      public $lastname;
-      public $email;
-      public $vendor_token = "58385365be57f651843810";
-      public $amount;
-      public $currency = "EUR";
-      public $error;
-      public $request_id;
-      public $message;
-
-      // public function __construct($firstname, $lastname, $email, $amount, $error, $request_id, $message) {
-      //   $this->firstname = $firstname;
-      //   $this->lastname = $lastname;
-      //   $this->email = $email;
-      //   $this->amount = $amount;
-      //   $this->error = $error;
-      //   $this->request_id = $request_id;
-      //   $this->message = $message;
-      // }
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $vendor_token = "58385365be57f651843810";
+    public $amount;
+    public $currency = "EUR";
+    public $error;
+    public $request_id;
+    public $message;
     }
 ?>
